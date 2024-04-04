5b09ecb58165a30c543785acc4979f5d964726ca
Tim Menzies <timm@ieee.org>
2024-04-04 14:37:35 -0400

    changed to better obect init scheme

--- less.lua
+++ less.lua
@@ -10,11 +10,11 @@ local max,min,abs,log,cos,sqrt = math.max,math.min,math.abs,math.log,math.cos,ma
 ----------------------------------------------------------------------------------------------------
 function NUM:init(str,int)  --> NUM.
   return {at=int,txt=str,n=0,mu=0,m2=0,lo=-BIG,hi=BIG,
-          heaven=(s or " "):find"-$" and 0 or 1} end
+          heaven=(str or " "):find"-$" and 0 or 1} end
 
 function NUM:add(num,    d) --> nil.
   if num~="?" then
-    self.n  = self.n + 1
+    self.n  = 1 + self.n 
     self.lo = min(num, self.lo)
     self.hi = max(num, self.hi)
     d       = num - self.mu
@@ -22,15 +22,31 @@ function NUM:add(num,    d) --> nil.
     self.m2 = self.m2 + d*(num - self.mu) 
     self.sd = self.n<2 and 0 or (self.m2/(self.n - 1))^.5 end end
 
+function NUM:mid() return self.mu end
+function NUM:div() return self.sd end
+
 function NUM:cdf(num,    z) --> float. Uses an approximation from S. Bowling JIEM 2009
-  z = (x - self.mu)/(self.sd + 1/BIG); return 1/(1 + E^(-1.702*z)) end
+  z = (num - self.mu)/(self.sd + 1/BIG); return 1/(1 + E^(-1.65451*z)) end
 ----------------------------------------------------------------------------------------------------
-function SYM.new(str,int) --> SYM.
-  return l.is(SYM, {at=int,txt=str,n=0,mu=0}) end
+function SYM:init(str,int) --> SYM.
+  return {at=int,txt=str,n=0,has={}} end
+
+function SYM:add(x,   n) --> nil.
+  if x~="?" then
+    n           = n or 1
+    self.n      = n + self.n 
+    self.has[x] = n + (self.has[x] or 0) end end
+
+function SYM:mid() --> x.
+  return l.mode(self.has) end
+
+function SYM:div() --> n. 
+  return l.entropy(self.has) end
 ----------------------------------------------------------------------------------------------------
-function COLS.new(strs,    all) -->  COLS.
-  all = kap(strs, function(int,str) return (str:find"^[A-Z]" and NUM or SYM)(str,int) end)
-  return l.is(COLS, {names=t, x={}, y={}, all=all}):categorized() end
+function COLS:init(strs,    all) -->  COLS.
+  all = l.kap(strs, function(int,str) return (str:find"^[A-Z]" and NUM or SYM)(str,int) end)
+  self.names, self.x, self.y , self.all = strs, {}, {}, all
+  self:categorized() end
 
 function COLS:catergorized(     t) --> COLS. Places columns   in their relevant categories..
   for _,col in pairs(self.all) do
@@ -40,36 +56,40 @@ function COLS:catergorized(     t) --> COLS. Places columns   in their relevant 
   return self end
 
 function COLS:add(t) --> t. Summaries the contents of `t`  within `self`.
-  for _,cols in pairs{i.x, i.y} do
+  for _,cols in pairs{self.x, self.y} do
     for _,col in pairs(cols) do
       col:add( t[col.at] ) end end
   return t end
 ----------------------------------------------------------------------------------------------------
-function DATA.new(src,  isOrdered,    self) --> DATA
-  self = l.is(DATA, {rows={},cols=nil}) 
+function DATA:init(src,  isOrdered,    self) --> DATA
+  self.rows, self.cols = {}, nil
   for t in src do self:add(t) end
-  if isOrdered then self.sort() end
-  return self end
+  if isOrdered then self.sort() end end
 
 function DATA:add(t)
   if   self.cols 
   then self.rows[1+#self.rows] = self.cols:add(t) 
-  else self.cols = COLS.new(t) end end
+  else self.cols = COLS(t) end end
 
 function DATA:sort()
-  l.keysort(i.rows, function(r) return self:d2h(r) end) end
+  return l.keysort(self.rows, function(r) return self:d2h(r) end) end
 
 function DATA:d2h(row,     n,d)
   n,d = 0,0
   for _,col in pairs(self.cols.y) do
     n = n + 1
-    d = d + abs(col:norm(row[i]) - col.heaven)^2 end
+    d = d + abs(col:norm(row[col.at]) - col.heaven)^2 end
   return (d/n)^.5 end
 ----------------------------------------------------------------------------------------------------
-function l.is(t,klass) --> t.
-  return setmetatable(klass,t) end
+function l.entropy(t,    e,N) --> n.
+  N=0; for _,n in pairs(t) do N = N+n end
+  e=0; for _,n in pairs(t) do if n>0 then e = e - n/N*log(n/N,2) end end
+  return e,N end
 
-function l.init(t) --> fun. Fun calls `t.new` with a thing of class t. Returns thing of same class.
+function l.mode(t,     x,n)
+  x,n=0,0; for x1,n1 in pairs(t) do if n1>n then x,n = x1,n1 end end; return x end
+
+function l.init(t) --> fun. Fun calls `t.init` with a thing of class t. Returns thing of same class.
   return function(_,...)
     local i = setmetatable({},t)
     return setmetatable(t.init(i,...) or i,t) end  end
@@ -80,7 +100,7 @@ function l.klassify(ts) --> nil. Enable methods. Let classes print themselves an
      t.__tostring= function(...) return str..l.kat(...) end 
      setmetatable(t, {__call = l.init(t)}) end end
 
-l.fmt = string.format 
+l.fmt = string.format
 
 function l.sort(t,fun) table.sort(t,fun); return t end
 
@@ -97,15 +117,15 @@ function l.dat(t) --> str of a tables with numeric indexes
 function l.kat(t) --> str of a table with keys, sorted by keys
   return l.dat(l.sort(l.kap(t, function (k,v) return l.fmt("%s=%s",k, l.rnd(v)) end))) end
 
-function l.rnd(x, nDecs) --> num rounded to `nDec`imal places
+function l.rnd(x, nDecs,    mult) --> num rounded to `nDec`imal places
   if type(x) ~= "number" then return x end
   if math.floor(x) == x  then return x end
   mult = 10^(nDecs or the.decimals)
   return math.floor(x * mult + 0.5) / mult end
 
-function l.items(t,    n)
-  i,max = 0,#t
-  return function() if i < max then i=i+1; return t[i] end end end 
+function l.items(t,    i,hi)
+  i,hi = 0,#t
+  return function() if i < hi then i=i+1; return t[i] end end end 
 
 function l.coerce(s)
   return math.tointeger(s) or tonumber(s) or s=="true" or (s~="false" and s) end
@@ -127,8 +147,8 @@ function eg.num(   n)
 
 function eg.norm(  n)
   n=NUM()
-  for j=1,10000 do n:add( (10 + 2 * sqrt(-2*log(R())) * cos(2*PI*R()))) end
-  for j=8,12,.25 do print(j, n:cdf(j)) end end
+  for j=1,10000 do n:add( (0 + 1 * sqrt(-2*log(R())) * cos(2*PI*R()))) end
+  for j=-3.5,0,.1 do print(j, n:cdf(j)) end end
 
 function eg.items()
   for x in l.items{10,20,30,40} do print(x) end end
